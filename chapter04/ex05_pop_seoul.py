9420023a53de41b8437cc2cde002d2066341e2be
Peter J <peter.j@kakao.com>
2020-09-14 19:33:36 +0000

    improve chapter04 examples

--- chapter04/ex05_pop_seoul.py
+++ chapter04/ex05_pop_seoul.py
@@ -2,18 +2,6 @@ import os, re, usecsv
 
 total = usecsv.opencsv('popSeoul.csv')
 
-#k = []
-for i in total[:5]:
-    #tmp = []
-    for j in i:
-        if re.search('[a-z가-힣]', j):
-            i[i.index(j)] = j
-            #tmp.append(j)
-        else:
-            i[i.index(j)] = float(re.sub(',', '', j))
-            #tmp.append(float(re.sub(',', '', j)))
-    #k.append(tmp)
-    print(i)
+result = usecsv.switch(total)
 
-#print(k)
-print(total[:5])
+print(result[:5])
